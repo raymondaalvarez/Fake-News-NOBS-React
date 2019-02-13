e917f4882c33d73f9ce3e8058061434d0a3a1fcb
ishootrayguns <alvarezraymond.a@gmail.com>
2019-02-12 22:53:31 -0800

    Updates on UI

--- fake-news-final/src/App.js
+++ fake-news-final/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { ReactComponent as Logo } from './NOBS Web Logo.svg';
 import './App.css';
-import { Jumbotron, Button } from 'reactstrap';
-import ReactSearchBox from 'react-search-box';
+import {Button, Navbar, Nav, FormControl ,Form, Container, Row, Col, Image} from 'react-bootstrap';
+import { ReactComponent as Logo } from './NOBS Web Logo.svg';
+import background from './background.jpg';
 
 
 
@@ -11,15 +11,27 @@ class App extends Component {
 
 
       return (
-        <div>
-          <Jumbotron style={{backgroundColor:"#73dd8d"}}>
-            <Logo className="stretch" style={{height:'50%', width:"50%",  display:'block', marginLeft:'auto', marginRight:'auto'}}>
-            </Logo>
-            <ReactSearchBox style={{display:'inline'}} placeholder="Enter article url" value="Search" data={this.data} callback={record => console.log(record)}>
-            </ReactSearchBox>
-            <button className="Display-1" style={{textAlign:"center"}}>SEARCH</button>
-          </Jumbotron>
-        </div>
+        <Container fluid style={{minHeight:'100vh',maxHeight:'999px', backgroundSize: '100% 100%', backgroundImage: `url(${background})`}}>
+          <Row>
+            <Navbar bg="dark" variant="dark">
+              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+              </Nav>
+            </Navbar>
+          </Row>
+          <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
+              <Logo style={{display:'inline-block', height:'70%', width:'70%',maxHeight:'400px',maxWidth:'400px'}}></Logo>
+          </Row>
+          <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
+              <Form>
+                      <FormControl style={{display:'inline-block', margin: '0 auto', width:'50%', padding:"10px"}} type="text" placeholder="Search" className="text-center"/>
+                      <Button variant="outline-info">Search</Button>
+              </Form>
+          </Row>
+        </Container>
 
       );
     }
