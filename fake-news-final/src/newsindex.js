d426193210ed0cff73e611f2831a595d20121ab5
ishootrayguns <alvarezraymond.a@gmail.com>
2019-02-19 13:03:50 -0800

    newsApi addition

---
+++ fake-news-final/src/newsindex.js
@@ -0,0 +1,54 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('9e3ac01f44214b3e8f0bcf5c572dc0de');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query /v2/everything
+// You must include at least one q, source, or domain
+newsapi.v2.everything({
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2017-12-01',
+  to: '2017-12-12',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+// To query sources
+// All options are optional
+newsapi.v2.sources({
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      sources: [...]
+    }
+  */
+});
