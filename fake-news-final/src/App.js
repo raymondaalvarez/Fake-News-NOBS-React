f4dc9ef41ed4de3a5b0d62f6537b2e5afdbe7ebc
ishootrayguns <alvarezraymond.a@gmail.com>
2019-02-19 12:51:51 -0800

    front-end update

--- fake-news-final/src/App.js
+++ fake-news-final/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button, Navbar, Nav, FormControl ,Form, Container, Row, Col, Image} from 'react-bootstrap';
+import {Button, Navbar, Nav, FormControl ,Form, Container, Row, Col, Image, NavItem} from 'react-bootstrap';
 import { ReactComponent as Logo } from './NOBS Web Logo.svg';
-import background from './background.jpg';
+import background from './background.png';
+import NavLink from 'react-bootstrap/NavLink';
 
 
 
@@ -11,26 +12,34 @@ class App extends Component {
 
 
       return (
-        <Container fluid style={{minHeight:'100vh',maxHeight:'999px', backgroundSize: '100% 100%', backgroundImage: `url(${background})`}}>
-          <Row>
-            <Navbar bg="dark" variant="dark">
-              <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-              <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-              </Nav>
+
+        <Container fluid>
+            <Navbar variant="success">
+                <img
+                  alt=""
+                  src="/NOBS Web Logo.svg"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />
+                {' NOBULLSHIT'}
+                  <NavLink>Home</NavLink>
+                  <NavLink>Videos</NavLink>
+                  <NavLink>Shows</NavLink>
+
             </Navbar>
-          </Row>
-          <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
-              <Logo style={{display:'inline-block', height:'70%', width:'70%',maxHeight:'400px',maxWidth:'400px'}}></Logo>
-          </Row>
-          <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
-              <Form>
-                      <FormControl style={{display:'inline-block', margin: '0 auto', width:'50%', padding:"10px"}} type="text" placeholder="Search" className="text-center"/>
-                      <Button variant="outline-info">Search</Button>
-              </Form>
-          </Row>
+          <Container style={{minHeight:'100vh',maxHeight:'999px', backgroundSize: '100% 100%', backgroundImage: `url(${background})`}}>
+
+            <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
+                <Logo style={{display:'inline-block', height:'70%', width:'70%',maxHeight:'400px',maxWidth:'400px'}}></Logo>
+            </Row>
+            <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
+                <Form>
+                        <FormControl style={{display:'inline-block', margin: '0 auto', width:'50%', padding:"10px"}} type="text" placeholder="Search" className="text-center"/>
+                        <Button variant="outline-info">Search</Button>
+                </Form>
+            </Row>
+          </Container>
         </Container>
 
       );
