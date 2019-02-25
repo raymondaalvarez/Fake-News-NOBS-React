3942721a0480e35c7b665ee0d912539931061cbf
ishootrayguns <alvarezraymond.a@gmail.com>
2019-02-25 00:39:42 -0800

    Changes to design

--- fake-news-final/src/App.js
+++ fake-news-final/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button, Navbar, Nav, FormControl ,Form, Container, Row, Col, Image, NavItem} from 'react-bootstrap';
+import {Button, Navbar, Nav, FormControl ,Form, Container, Row, Col, Image, NavItem, Card, Table} from 'react-bootstrap';
 import { ReactComponent as Logo } from './NOBS Web Logo.svg';
 import background from './background.png';
 import NavLink from 'react-bootstrap/NavLink';
@@ -8,37 +8,72 @@ import NavLink from 'react-bootstrap/NavLink';
 
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    console.log("This is my initializer")
+
+    const article = { id:'0', title: 'Best of Late Night: Stephen Colbert Wants to Remind You: Trump Isn’t Normal', description:"That mildly nauseous feeling you have is because for the last two years, Donald Trump has been spinning you in a tumble-dryer full of turd,” Colbert said."
+    }
+
+    this.state = {rows:
+      <Table bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Article</th>
+            <th>Truth Rating</th>
+            <th>Fake News Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>
+              <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+              </Card>
+            </td>
+            <td>7/10</td>
+            <td>2/10</td>
+          </tr>
+        </tbody>
+
+      </Table>
+
+    }
+
+  }
+
   render() {
 
 
       return (
 
         <Container fluid>
-            <Navbar variant="success">
-                <img
-                  alt=""
-                  src="/NOBS Web Logo.svg"
-                  width="30"
-                  height="30"
-                  className="d-inline-block align-top"
-                />
-                {' NOBULLSHIT'}
-                  <NavLink>Home</NavLink>
-                  <NavLink>Videos</NavLink>
-                  <NavLink>Shows</NavLink>
-
-            </Navbar>
-          <Container style={{minHeight:'100vh',maxHeight:'999px', backgroundSize: '100% 100%', backgroundImage: `url(${background})`}}>
-
-            <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
-                <Logo style={{display:'inline-block', height:'70%', width:'70%',maxHeight:'400px',maxWidth:'400px'}}></Logo>
-            </Row>
+          <Container className="fn-header" style={{backgroundImage: `url(${background})`}}>
+            <Table className='App-header' style={{width:'100%',margin:'0 auto'}}>
+              <tbody>
+                <tr>
+                  <td><Logo className="App-logo"></Logo></td>
+                  <td><h3>Fake News Search</h3></td>
+                </tr>
+              </tbody>
+            </Table>
             <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
                 <Form>
-                        <FormControl style={{display:'inline-block', margin: '0 auto', width:'50%', padding:"10px"}} type="text" placeholder="Search" className="text-center"/>
-                        <Button variant="outline-info">Search</Button>
+                        <FormControl style={{display:'block', margin: '0px', width:'99%', paddingTop:"8px", paddingBottom:"8px", paddingLeft:"8px", contentAlign:"center"}} type="text" placeholder="Search" className="text-center"/>
+                        <Button>Search</Button>
                 </Form>
             </Row>
+            {this.state.rows}
           </Container>
         </Container>
 
