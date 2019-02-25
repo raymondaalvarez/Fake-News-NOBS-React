2e01cf20d4aa2ef48cad22423e7a9a9c4f1629d6
ishootrayguns <alvarezraymond.a@gmail.com>
2019-02-25 02:33:56 -0800

    Card progression

--- fake-news-final/src/App.js
+++ fake-news-final/src/App.js
@@ -11,44 +11,36 @@ class App extends Component {
   constructor(props){
     super(props)
     console.log("This is my initializer")
+    var articleRows = []
+    const article =[
+      { id:'0', title: 'Best of Late Night: Stephen Colbert Wants to Remind You: Trump Isn’t Normal', description:"That mildly nauseous feeling you have is because for the last two years, Donald Trump has been spinning you in a tumble-dryer full of turd,” Colbert said.", content: "Seth Meyers pointed to a new argument thats becoming popular on Fox News: that perhaps Trump shouldnt declare a national emergency to build the border wall (a move that could well be struck down by federal courts anyway, experts believe) because then a future…", imgUrl:"https://static01.nyt.com/images/2019/02/21/arts/21latenight1/21latenight1-facebookJumbo.png"},
+      { id:'1', title: 'Trump picks new UN ambassador', description:"President Donald Trump said Friday he intends to nominate the current American ambassador to Canada to be the new US ambassador to the United Nations.", content: "Chat with us in Facebook Messenger. Find out what's happening in the world as it unfolds.", imgUrl:"https://cdn.cnn.com/cnnnext/dam/assets/190220165717-trump-un-super-tease.jpg"}
+  ]
 
-    const article = { id:'0', title: 'Best of Late Night: Stephen Colbert Wants to Remind You: Trump Isn’t Normal', description:"That mildly nauseous feeling you have is because for the last two years, Donald Trump has been spinning you in a tumble-dryer full of turd,” Colbert said."
-    }
+    article.forEach(article => {
+      console.log(article.title);
+      const articleRow =
+        <Table bordered key={article.id}>
+          <tbody className="table-body">
+            <tr>
+              <td className="table-row">
+                <Card className="bg-dark text-white">
+                  <Card.Img className="rounded-corner" height={150} width={200} src={article.imgUrl} alt="Card image" />
+                </Card>
+              </td>
+              <td className="table-row">
+                <h3 className="App-font">{article.title}</h3>
+                <p className="App-font-p">{article.description}</p>
+                <Button className='App-button'>Statistics</Button>
+                <Button className='App-button'>Read</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      articleRows.push(articleRow);
+    })
 
-    this.state = {rows:
-      <Table bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Article</th>
-            <th>Truth Rating</th>
-            <th>Fake News Rating</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>
-              <Card style={{ width: '18rem' }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-              </Card>
-            </td>
-            <td>7/10</td>
-            <td>2/10</td>
-          </tr>
-        </tbody>
-
-      </Table>
-
-    }
+    this.state = {rows: articleRows}
 
   }
 
