db00f28062123ab8a21f55b0666f7a87f0f6b655
ishootrayguns <alvarezraymond.a@gmail.com>
2019-03-11 03:07:14 -0700

    Update and regular search works still need to parse url

--- fake-news-final/src/App 2.js
+++ fake-news-final/src/App 2.js
@@ -5,6 +5,7 @@ import { ReactComponent as Logo } from './NOBS Web Logo.svg';
 import { BrowserRouter as Router, Switch, Link, Route} from "react-router-dom";
 import Loading from "./loading";
 import Home from "./home";
+<<<<<<< HEAD
 import $ from 'jquery';
 import ArticleRow from './ArticleRow';
 const NewsAPI = require('newsapi');
@@ -12,12 +13,31 @@ const newsapi = new NewsAPI('9e3ac01f44214b3e8f0bcf5c572dc0de');
 
 
 
+=======
+import Results from "./results";
+import { withRouter } from 'react-router';
+import $ from "jquery";
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('9e3ac01f44214b3e8f0bcf5c572dc0de');
+>>>>>>> f7f1afda9272c947e0953b6e2618f224b40b5e8f
 
+var articleTitle = '';
+var articleContent = '';
 
 class App extends Component {
   constructor(props ){
     super(props)
+<<<<<<< HEAD
     this.state = {rows: [], value: ''};
+=======
+    var articleRows = []
+    const article =[
+      { id:'0', title: 'Best of Late Night: Stephen Colbert Wants to Remind You: Trump Isn’t Normal', description:"That mildly nauseous feeling you have is because for the last two years, Donald Trump has been spinning you in a tumble-dryer full of turd,” Colbert said.", content: "Seth Meyers pointed to a new argument thats becoming popular on Fox News: that perhaps Trump shouldnt declare a national emergency to build the border wall (a move that could well be struck down by federal courts anyway, experts believe) because then a future…", imgUrl:"https://static01.nyt.com/images/2019/02/21/arts/21latenight1/21latenight1-facebookJumbo.png"},
+      { id:'1', title: 'Trump picks new UN ambassador', description:"President Donald Trump said Friday he intends to nominate the current American ambassador to Canada to be the new US ambassador to the United Nations.", content: "Chat with us in Facebook Messenger. Find out what's happening in the world as it unfolds.", imgUrl:"https://cdn.cnn.com/cnnnext/dam/assets/190220165717-trump-un-super-tease.jpg"}
+  ]
+
+    this.state = {rows: articleRows, value: ''}
+>>>>>>> f7f1afda9272c947e0953b6e2618f224b40b5e8f
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
@@ -58,6 +78,9 @@ handleSubmit(event){
   console.log("Input is: " + this.state.value);
   if (this.state.value.includes('www.')){
     console.log("TRUE");
+    var prom = new Promise(resolve, reject) {
+
+    }
     console.log(this.getArticleData(this.state.value));
     this.performSearch(this.getArticleData(this.state.value));
   }
@@ -79,7 +102,21 @@ getArticleData(link){
     //console.log(data);
     var el = document.createElement('html');
     el.innerHTML=data;
+<<<<<<< HEAD
     return el.getElementsByTagName('title')[0].innerHTML;
+=======
+    //console.log(el.getElementsByTagName('title')[0]);
+    articleTitle = el.getElementsByTagName('title')[0].innerHTML;
+    console.log(articleTitle);
+    newsapi.v2.topHeadlines({
+      q: articleTitle
+    }).then(response => {
+      articleContent = response.articles[0].content;
+      console.log(response.articles[0].content);
+    });
+    }).catch(function (response) {
+      console.log(response);
+>>>>>>> f7f1afda9272c947e0953b6e2618f224b40b5e8f
     });
   });
   return val;
@@ -98,11 +135,18 @@ getArticleData(link){
             </tbody>
           </Table>
           <Row style={{width:'100%',margin:'0 auto',textAlign:'center'}}>
+<<<<<<< HEAD
               <Form onSubmit={this.handleSubmit}>
                       <input value={this.state.value} onChange={this.handleChange} style={{display:'block', margin: '0px', width:'99%', paddingTop:"8px", paddingBottom:"8px", paddingLeft:"8px", contentAlign:"center"}} type="text" placeholder="Enter article URL"/>
                       <input type="submit" value="Submit" />
                       {this.state.rows}
               </Form>
+=======
+              <form onSubmit={this.handleSubmit}>
+                      <Link to={"/results"} className="nav-link" replace><input value={this.state.value} onChange={this.handleChange} style={{display:'block', margin: '0px', width:'99%', paddingTop:"8px", paddingBottom:"8px", paddingLeft:"8px", contentAlign:"center"}} type="text" placeholder="Enter article URL"/></Link>
+                      <input type="submit" value="Submit" />
+              </form>
+>>>>>>> f7f1afda9272c947e0953b6e2618f224b40b5e8f
           </Row>
             <Switch>
                 <Route exact path="/" component={Home}/>
@@ -115,6 +159,7 @@ getArticleData(link){
 }
 
 export default App;
+export {articleTitle};
 //The input is stored in this.state.value
 
 
@@ -124,4 +169,3 @@ export default App;
 
 //I took this out for now since it is more of a template for when we get article info from the search
 //{this.state.rows}
-
